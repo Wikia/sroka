a41ca88719a70bef8f400577d94794e0f94227ae
Dorota Mierzwa <mierzwa.dorota@gmail.com>
2019-08-08 08:40:48 +0200

    authentication error update

--- sroka/api/google_ad_manager/gam_api.py
+++ sroka/api/google_ad_manager/gam_api.py
@@ -60,7 +60,9 @@ def get_data_from_admanager(query, dimensions, columns, start_date, end_date, cu
         # Run the report and wait for it to finish.
         report_job_id = report_downloader.WaitForReport(report_job)
     except errors.GoogleAdsServerFault as e:
-        if 'AuthenticationError' in str(e):
+        if 'AuthenticationError.NETWORK_NOT_FOUND' in str(e):
+            print('Provided network code was not found.')
+        elif 'AuthenticationError.NETWORK_CODE_REQUIRED' in str(e):
             print('Default value of network code is missing from ', config.default_config_filepath)
         else:
             print('Failed to generate report. Error was: {}'.format(e))
