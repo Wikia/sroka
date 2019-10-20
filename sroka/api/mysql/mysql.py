8a3323493fcc7e368315f6a5e4717c40ea219fe8
Ullaakut <brendan.le-glaunec@epitech.eu>
2019-10-20 08:45:23 +0200

    Improve exception handling

--- sroka/api/mysql/mysql.py
+++ sroka/api/mysql/mysql.py
@@ -19,27 +19,30 @@ def query_mysql(query: str, filename=None):
         print('Missing MySQL section in configuration')
         return pd.DataFrame([])
 
-    if validate_options(options):
+    if not validate_options(options):
         return pd.DataFrame([])
 
-        # Connect while passing only the arguments that were set in the
-    # configuration, since some are mutually exclusive or optional.
-    connection = mysql.connector.connect(**{k: v for k, v in options.items() if v != ""})
-
-    # Get the MySQL connector cursor, which allows interacting with the
-    # MySQL server.
-    cursor = connection.cursor()
-
-    # Execute the given query.
     try:
+        # Connect while passing only the arguments that were set in the
+        # configuration, since some are mutually exclusive or optional.
+        connection = mysql.connector.connect(**{k: v for k, v in options.items() if v != ""})
+
+        # Get the MySQL connector cursor, which allows interacting with the
+        # MySQL server.
+        cursor = connection.cursor()
+
+        # Execute the query.
         cursor.execute(query)
 
     except OperationalError as e:
         print('Operational MySQL Error: {}'.format(e))
+        return pd.DataFrame([])
     except InternalError as e:
         print('Internal MySQL Error: {}'.format(e))
+        return pd.DataFrame([])
     except DatabaseError as e:
         print('Database MySQL Error: {}'.format(e))
+        return pd.DataFrame([])
 
     # Cycle through the returned elements to build a pandas DataFrame.
     df = pd.DataFrame(cursor, columns=cursor.column_names)
@@ -62,36 +65,31 @@ def get_options_from_config():
 
     try:
         options["host"] = config.get_value('mysql', 'host')
-
     except (KeyError, NoOptionError):  # Do nothing, this value is optional.
         pass
 
     try:
         options["port"] = config.get_value('mysql', 'port')
-
     except (KeyError, NoOptionError):  # Do nothing, this value is optional.
         pass
+
     try:
         options["user"] = config.get_value('mysql', 'user')
-
     except (KeyError, NoOptionError):  # Do nothing, this value is optional.
         pass
 
     try:
         options["password"] = config.get_value('mysql', 'password')
-
     except (KeyError, NoOptionError):  # Do nothing, this value is optional.
         pass
 
     try:
         options["unix_socket"] = config.get_value('mysql', 'unix_socket')
-
     except (KeyError, NoOptionError):  # Do nothing, this value is optional.
         pass
 
     try:
         options["database"] = config.get_value('mysql', 'database')
-
     except (KeyError, NoOptionError):  # Do nothing, this value is optional.
         pass
 
@@ -106,6 +104,7 @@ def validate_options(options: dict):
     if "unix_socket" not in options and "host" not in options:
         print('Invalid Configuration: In order to connect to the MySQL database, the host/port options or the unix_socket option must be set.')
         return False
+
     # If both keys are present and both have values, the configuration is
     # ambiguous.
     if "unix_socket" in options and "host" in options and \
