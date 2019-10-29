402d765f209247bfe6f7773eb0e3ce100a7a27a0
martynaut <martyna.urbanek@gmail.com>
2019-10-29 14:25:43 +0100

    athena error message fix

--- sroka/api/athena/athena_api.py
+++ sroka/api/athena/athena_api.py
@@ -17,20 +17,25 @@ def poll_status(session, _id):
         result = session.get_query_execution(
             QueryExecutionId=_id
         )
-    except ClientError:
-        print("Invalid query_id")
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'InvalidRequestException':
+            print("Please check your query_id. Error message:")
+        else:
+            print("ClientError. Error message:")
+        print(e)
         return None
-    except EndpointConnectionError:
-        print('Please check your credentials including aws_region in config.ini file')
+    except EndpointConnectionError as e:
+        print('Please check your credentials including aws_region in config.ini file and Internet connection.',
+              'Error message:')
+        print(e)
         return None
 
     state = result['QueryExecution']['Status']['State']
 
-    if state == 'SUCCEEDED':
-        return result
-    elif state == 'FAILED':
+    if state in ('SUCCEEDED', 'FAILED', 'CANCELLED'):
         return result
     else:
+        print('Waiting for {} query to end'.format(_id))
         raise Exception
 
 
@@ -38,11 +43,18 @@ def download_file(s3, s3_bucket, s3_key, filename):
     if filename:
         try:
             s3.Bucket(s3_bucket).download_file(s3_key, filename)
-        except FileNotFoundError:
-            print('File or folder not found.')
+        except FileNotFoundError as e:
+            print('File or folder not found. Error message:')
+            print(e)
             return None
-        except ClientError:
-            print('Please check your credentials including s3_bucket in config.ini file')
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'InvalidRequestException':
+                print("Please check your query. Error message:")
+            elif e.response['Error']['Code'] == '':
+                print('')
+            else:
+                print('Please check your credentials including s3_bucket in config.ini file. Error message:')
+            print(e)
             return None
         print('saved to ' + filename)
         return None
@@ -50,9 +62,13 @@ def download_file(s3, s3_bucket, s3_key, filename):
         obj = s3.Object(s3_bucket, s3_key)
         try:
             obj = obj.get()
-        except ClientError:
-            print('Please check your credentials including s3_bucket in config.ini file')
-            return pd.DataFrame([])
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'InvalidRequestException':
+                print("Please check your query. Error message:")
+            else:
+                print('Please check your credentials including s3_bucket in config.ini file. Error message:')
+            print(e)
+            return None
         df = pd.read_csv(obj['Body'])
         return df
 
@@ -63,9 +79,13 @@ def query_athena(query, filename=None):
         key_id = config.get_value('aws', 'aws_access_key_id')
         access_key = config.get_value('aws', 'aws_secret_access_key')
         region = config.get_value('aws', 'aws_region')
-    except (KeyError, NoOptionError):
-        print('No credentials were provided')
-        return pd.DataFrame([])
+    except (KeyError, NoOptionError) as e:
+        print('No credentials were provided. Error message:')
+        print(e)
+        if filename:
+            return None
+        else:
+            return pd.DataFrame([])
 
     session = boto3.Session(
         aws_access_key_id=key_id,
@@ -87,19 +107,41 @@ def query_athena(query, filename=None):
                 'OutputLocation': output_s3_bucket,
             }
         )
-    except ClientError:
-        print('Please check your credentials including s3_bucket in config.ini file')
-        return pd.DataFrame([])
-    except EndpointConnectionError:
-        print('Please check your credentials including aws_region in config.ini file')
-        return pd.DataFrame([])
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'InvalidRequestException':
+            print("Please check your query. Error message:")
+        else:
+            print('Please check your credentials including s3_bucket in config.ini file. Error message:')
+        print(e)
+        if filename:
+            return None
+        else:
+            return pd.DataFrame([])
+    except EndpointConnectionError as e:
+        print('Please check your credentials including aws_region in config.ini file and Internet connection.',
+              'Error message:')
+        print(e)
+        if filename:
+            return None
+        else:
+            return pd.DataFrame([])
     query_id = result['QueryExecutionId']
     result = poll_status(athena, query_id)
-    if result['QueryExecution']['Status']['State'] == 'SUCCEEDED':
+    if result is None:
+        if filename:
+            return None
+        else:
+            return pd.DataFrame([])
+    elif result['QueryExecution']['Status']['State'] == 'SUCCEEDED':
         s3_key = query_id + '.csv'
         return download_file(s3, s3_bucket, s3_key, filename)
     else:
-        print('Query did not succeed')
+        print('Query did not succeed. Reason:')
+        print(result['QueryExecution']['Status']['StateChangeReason'])
+        if filename:
+            return None
+        else:
+            return pd.DataFrame([])
 
 
 def done_athena(query_id, filename=None):
@@ -108,9 +150,14 @@ def done_athena(query_id, filename=None):
         key_id = config.get_value('aws', 'aws_access_key_id')
         access_key = config.get_value('aws', 'aws_secret_access_key')
         region = config.get_value('aws', 'aws_region')
-    except (KeyError, NoOptionError):
-        print('No credentials were provided')
-        return pd.DataFrame([])
+    except (KeyError, NoOptionError) as e:
+        print('All or part of credentials were not provided. Please verify config.ini file. Error message:')
+        print(e)
+        if filename:
+            return None
+        else:
+            return pd.DataFrame([])
+
     if s3_bucket.startswith('s3://'):
         s3_bucket = s3_bucket.replace('s3://', '')
 
@@ -124,9 +171,14 @@ def done_athena(query_id, filename=None):
                             region_name=region)
     result = poll_status(athena, query_id)
     if result is None:
-        return pd.DataFrame([])
+        if filename:
+            return None
+        else:
+            return pd.DataFrame([])
     if result['QueryExecution']['Status']['State'] == 'SUCCEEDED':
         s3_key = urlparse(result['QueryExecution']['ResultConfiguration']['OutputLocation']).path[1:]
         return download_file(s3, s3_bucket, s3_key, filename)
     else:
-        print('Query did not succeed')
+        print('Query did not succeed. Reason:')
+        print(result['QueryExecution']['Status']['StateChangeReason'])
+        return pd.DataFrame([])
